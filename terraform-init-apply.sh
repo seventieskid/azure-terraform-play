f1525b9b9790d60c83d99f3b57379a87aec12c55
Gareth Rees <grees@azuron.co.uk>
2024-12-16 16:20:14 +0000

    terraform init refinements

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -1,5 +1,25 @@
-az account show
+#!/bin/bash
 
-terraform init
+RESOURCE_GROUP_NAME=azuron-play-eu-west
+STORAGE_ACCOUNT_NAME=azuronplayeuwest
+CONTAINER_NAME=terraforma-zuron-play-eu-west
+REGION=westeu
+
+# # Create resource group
+# az group create --name $RESOURCE_GROUP_NAME --location $REGION
+
+# # Create storage account
+# az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+
+# See https://developer.hashicorp.com/terraform/language/backend/azurerm
+terraform init -backend=true \
+               -backend-config="resource_group_name=azuron-play-eu-west" \
+               -backend-config="storage_account_name=azuronplayeuwest" \
+               -backend-config="container_name=terraforma-zuron-play-eu-west"  \
+               -backend-config="use_azuread_auth=true"  \
+               -backend-config="use_oidc=true"
 
 terraform apply -auto-approve
