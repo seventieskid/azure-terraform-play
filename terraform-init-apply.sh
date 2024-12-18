f72f2fe42b822d3c139245bcccf7428268ba5619
Gareth Rees <grees@azuron.co.uk>
2024-12-18 15:37:25 +0000

    test

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -2,22 +2,25 @@
 
 az login --identity
 
-# # Create resource group
-# az group create --name $RESOURCE_GROUP_NAME --location $REGION
+RESOURCE_GROUP=frominsidecontainer
+REGION=westeurope
 
-# # Create storage account
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location $REGION
+
+# Create storage account
 # az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --auth-mode login
+# # Create blob container
+# az storage container create --name xyz --account-name $STORAGE_ACCOUNT_NAME
 
-# See https://developer.hashicorp.com/terraform/language/backend/azurerm
-terraform init -backend=true \
-               -backend-config="resource_group_name=$RESOURCE_GROUP_NAME" \
-               -backend-config="storage_account_name=$STORAGE_ACCOUNT_NAME" \
-               -backend-config="container_name=$CONTAINER_NAME"  \
-               -backend-config="key=terraform.tfstate"  \
-               -backend-config="use_azuread_auth=true"  \
-               -backend-config="use_oidc=true"
+# # See https://developer.hashicorp.com/terraform/language/backend/azurerm
+# terraform init -backend=true \
+#                -backend-config="resource_group_name=$RESOURCE_GROUP_NAME" \
+#                -backend-config="storage_account_name=$STORAGE_ACCOUNT_NAME" \
+#                -backend-config="container_name=$CONTAINER_NAME"  \
+#                -backend-config="key=terraform.tfstate"  \
+#                -backend-config="use_azuread_auth=true"  \
+#                -backend-config="use_oidc=true"
 
-terraform apply -auto-approve
+# terraform apply -auto-approve
