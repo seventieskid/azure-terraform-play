eb8f6f4977073f3aba82638b42c18bece547b1c2
Gareth Rees <grees@azuron.co.uk>
2024-12-19 19:27:05 +0000

    typos

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -1,17 +1,13 @@
 #!/bin/bash 
 
 RESOURCE_GROUP_NAME=terraform-test-rg
-STORAGE_ACCOUNT_NAME=terraform-test-storage-ac
+STORAGE_ACCOUNT_NAME=terraformteststorageac
 STORAGE_CONTAINER_NAME=terraform-state
 REGION=westeurope
 
-# Create resource group
+# Prep for tf state storage
 az group create --name $RESOURCE_GROUP_NAME --location $REGION
-
-Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
-
-# Create blob container
 az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
 # See https://developer.hashicorp.com/terraform/language/backend/azurerm
