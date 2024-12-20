cf590cadbfc1fb28225c498088f3743a04ddbdaa
Gareth Rees <grees@azuron.co.uk>
2024-12-20 16:20:12 +0000

    now storage container

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 RESOURCE_GROUP_NAME=terraform-test-rg
-STORAGE_ACCOUNT_NAME=terraform
+STORAGE_ACCOUNT_NAME=terraformgarethrees
 STORAGE_CONTAINER_NAME=terraform-state
 COMMIT_SHA=$LONG_COMMIT_SHA
 REGION=westeurope
@@ -9,9 +9,9 @@ REGION=westeurope
 # Prep for tf state storage
 az group create --name $RESOURCE_GROUP_NAME --location $REGION
 
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME"garethrees" --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
-# az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
 # See https://developer.hashicorp.com/terraform/language/backend/azurerm
 # terraform init -backend=true \
