fd69da15171763dd0e2e823df6c1461f44c680dd
Gareth Rees <grees@azuron.co.uk>
2024-12-20 16:27:13 +0000

    push sub id into env

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -13,7 +13,9 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 
 az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
-export ARM_USE_MSI=true ARM_TENANT_ID=576d634f-7729-4278-9174-4ed588ee532a
+export ARM_USE_MSI=true
+export ARM_TENANT_ID=576d634f-7729-4278-9174-4ed588ee532a
+export ARM_SUBSCRIPTION_ID=60e1436b-d08b-466d-b42a-98011fed3eb2
 
 # See https://developer.hashicorp.com/terraform/language/backend/azurerm
 terraform init -backend=true \
