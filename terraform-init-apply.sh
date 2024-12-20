9d06568be1cb6c0a65f057d044e2494ff177bca3
Gareth Rees <grees@azuron.co.uk>
2024-12-20 16:24:40 +0000

    try to force TF to use MSI

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -13,14 +13,14 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 
 az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
+export ARM_USE_MSI=true ARM_TENANT_ID=576d634f-7729-4278-9174-4ed588ee532a
+
 # See https://developer.hashicorp.com/terraform/language/backend/azurerm
 terraform init -backend=true \
                -backend-config="resource_group_name=$RESOURCE_GROUP_NAME" \
                -backend-config="storage_account_name=$STORAGE_ACCOUNT_NAME" \
                -backend-config="container_name=$STORAGE_CONTAINER_NAME"  \
                -backend-config="key=terraform.tfstate"
-
-#export ARM_USE_MSI=true ARM_TENANT_ID=576d634f-7729-4278-9174-4ed588ee532a
 
 # terraform init -backend=true \
 #                -backend-config="resource_group_name=$RESOURCE_GROUP_NAME" \
