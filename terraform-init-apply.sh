6d7be540a73fa2457cf20a10d602c41a811d846d
Gareth Rees <grees@azuron.co.uk>
2024-12-20 15:50:42 +0000

    simplify

--- terraform-init-apply.sh
+++ terraform-init-apply.sh
@@ -7,7 +7,9 @@ REGION=westeurope
 
 # Prep for tf state storage
 az group create --name $RESOURCE_GROUP_NAME --location $REGION
-# az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob --auth-mode login
+
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
 # az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
 # See https://developer.hashicorp.com/terraform/language/backend/azurerm
@@ -19,13 +21,13 @@ az group create --name $RESOURCE_GROUP_NAME --location $REGION
 
 #export ARM_USE_MSI=true ARM_TENANT_ID=576d634f-7729-4278-9174-4ed588ee532a
 
-terraform init -backend=true \
-               -backend-config="resource_group_name=$RESOURCE_GROUP_NAME" \
-               -backend-config="storage_account_name=manualtester" \
-               -backend-config="container_name=terraformstate"  \
-               -backend-config="key=terraform.tfstate"
+# terraform init -backend=true \
+#                -backend-config="resource_group_name=$RESOURCE_GROUP_NAME" \
+#                -backend-config="storage_account_name=manualtester" \
+#                -backend-config="container_name=terraformstate"  \
+#                -backend-config="key=terraform.tfstate"
 
-            #    -backend-config="use_azuread_auth=true"  \
-            #    -backend-config="use_oidc=true"
+#             #    -backend-config="use_azuread_auth=true"  \
+#             #    -backend-config="use_oidc=true"
 
-terraform apply -auto-approve
+# terraform apply -auto-approve
