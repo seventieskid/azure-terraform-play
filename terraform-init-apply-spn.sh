851401b52eb7e74a1d7f8e50912956f05991c1e8
Gareth Rees <grees@azuron.co.uk>
2024-12-23 17:49:56 +0000

    tidy comments

--- terraform-init-apply-spn.sh
+++ terraform-init-apply-spn.sh
@@ -14,13 +14,6 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 
 az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
 
-# Allow the service principal to get access keys for the storage account
-# az role assignment create \
-#     --role "Storage Account Contributor" \
-#     --assignee-object-id "3cb931d6-05df-4d9f-b3b7-adefd0b6db60" \
-#     --assignee-principal-type "ServicePrincipal" \
-#     --scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME/blobServices/default/containers/$STORAGE_CONTAINER_NAME"
-
 # Because the below is commented out, terraform is falling back on the SPN contents to run, not the MSI
 #export ARM_USE_MSI=true
 
